5a448625216eb413201e40f2a750f09343c172f1
AGarciaNY <aldairgarcia803@gmail.com>
2020-03-01 16:49:45 -0500

    survay is finished

---
+++ src/Components/survay/keys.js
@@ -0,0 +1,17 @@
+
+const questionkeys=[
+  "Gender",
+  "SexualOrientation",
+  "EthnicityRace",
+  "Language",
+  "DisablityStatus",
+  "VeteranStatus",
+  "CaregiverStatus",
+  "Sector",
+  "Religion",
+  "SocioeconomicStatus",
+  "ImmigtationStatus",
+  "HourlyWage",
+  "Hours"
+]
+export default questionkeys;
